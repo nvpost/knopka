44ca36e187a9ebc216c7dd6ee22b1c1419896783
nik <nvbpost@yandex.ru>
2016-10-25 16:09:36 +0300

    edit main

--- js.js
+++ js.js
@@ -5,74 +5,105 @@ var marginForBtn = document.documentElement.clientWidth/2-75;
 var counterName="Нет имени";
 var counterLogger=[];
 var t;
-
+var jC=''
+$('.buttonHolder').css('margin-left', marginForBtn)
 if(!localStorage.getItem('justCounter')){
 	var jC='';
 	//localStorage.setItem('justCounter', justCounter)
 }else{
 	var jC=localStorage.getItem('justCounter')
-}
-
-$('.buttonHolder').css('margin-left', marginForBtn)
-
-$('.tick').on('mousedown', function(){
-	counter()
-	if(boolStart){
-		sTime()
-		counterLogger.push(counterName)
- 		counterLogger.push(moment())
- 		timer()
+	jCarr=jC.split('/%/')
 	}
-	boolStart=false;
-	
-	logger()
-})
 
-function counter(){
-	mainCounter++;
-	$(".appConsole").find('p').text(mainCounter)
-}
-function sTime(){
-	startTime=moment()
-	$('.hStart').find('span').html(startTime.format("DD.MM <br> HH:mm:ss"))
-	return startTime;
-}
-function timer(){
-	var delta=moment()-startTime;
-	$('.hLong').find('span').html(moment(delta).format('mm:ss'))
-	t=setTimeout(timer, 1000)
-}
-//название
-$('.hName').on('click', function(){
-	counterName=prompt('Введите название')
-	counterLogger[0]=counterName
-	$('.hName').find('span').text(counterName)
-})
-//конец названия
-//Стоп машина
-$('.hLong').on('click', function(){
-	logger('s') //записываем конец
-	// функция записи кудато, возможно в локальное хранилище
 
-	
-	mainCounter=0
-	startTime=0
-	clearTimeout(t)
-	boolStart=true;
-	counterName="Нет имени";
-	counterLogger=[];
-	$('.hStart').find('span').html("для старта нажмите +1")
-	$(".appConsole").addClass('stopCounter')
-})
-//Конец стопа 
-function logger(s){
-	counterLogger.push(new Date().getTime())
-	if(s=='s'){
-		if(newName=prompt('Чтобы сохранить нажмите ОК и, при желании, введите название', counterName)){
-			counterLogger[0]=newName
-			jC=jC+"/%/"+counterLogger
-			localStorage.setItem('justCounter', jC)
-		}
+
+
+
+var idToChange = localStorage.getItem('idToChange')
+localStorage.removeItem('idToChange')
+if(idToChange){
+	addCounter(idToChange)
+}else{
+	start()
+
+	}
+
+
+
+
+function start(){
+
+
 		
-	}
+
+		$('.tick').on('mousedown', function(){
+			counter()
+			if(boolStart){
+				sTime()
+				counterLogger.push(counterName)
+		 		//counterLogger.push(moment())
+		 		timer()
+			}
+			boolStart=false;
+			
+			logger()
+		})
+
+		function counter(){
+			mainCounter++;
+			$(".appConsole").find('p').text(mainCounter)
+		}
+		function sTime(){
+			startTime=moment()
+			$('.hStart').find('span').html(startTime.format("DD.MM <br> HH:mm:ss"))
+			return startTime;
+		}
+		function timer(){
+			var delta=moment()-startTime;
+			$('.hLong').find('span').html(moment(delta).format('mm:ss'))
+			t=setTimeout(timer, 1000)
+		}
+		//название
+		$('.hName').on('click', function(){
+			counterName=prompt('Введите название')
+			counterLogger[0]=counterName
+			$('.hName').find('span').text(counterName)
+		})
+		//конец названия
+		//Стоп машина
+		$('.hLong').on('click', function(){
+			logger('s') //записываем конец
+			// функция записи кудато, возможно в локальное хранилище
+			mainCounter=0
+			startTime=0
+			clearTimeout(t)
+			boolStart=true;
+			counterName="Нет имени";
+			counterLogger=[];
+			$('.hStart').find('span').html("для старта нажмите +1")
+			$(".appConsole").addClass('stopCounter')
+		})
+		//Конец стопа 
+		function logger(s){
+			counterLogger.push(new Date().getTime())
+			if(s=='s'){
+				if(newName=prompt('Чтобы сохранить нажмите ОК и, при желании, введите название', counterName)){
+					counterLogger[0]=newName
+					jCarr.push(counterLogger)
+					jC=jCarr.join("/%/")
+					localStorage.setItem('justCounter', jC)
+				}
+			}
+		}
+
+}
+
+
+function addCounter(c){
+	console.log('Будем менть массив'+c)
+	$('.appCounterName').html(jCarr[c][0])
+	mainCounter=jCarr[c].split(',').length-1
+	$(".appConsole").find('p').text(mainCounter)
+	startTime=jCarr[c][1]
+	timer()
 }
--- stat.js
+++ stat.js
@@ -1,19 +1,107 @@
+
+
 var starArr=localStorage.getItem('justCounter').split('/%/')
-starArr.shift()
 $('#vsegoZapisey').html(starArr.length)
-var table="<table class='table table-bordered'> <thead><tr><th>Дата</th><th>Название</th><th>Кол-во</th></tr> </thead>"
+var table="<table class='table table-bordered'> <thead><tr><th>Re</th><th>Дата</th><th>Название</th><th>Кол-во</th></tr> </thead>"
 for(i in starArr){
 	var nArr=starArr[i].split(',')
- 	table=table+"<tr data-row="+i+"><td>"+moment(nArr[1]).format('DD.MM, HH:mm')+"</td><td>"+nArr[0]+"</td><td>"+(nArr.length-3)+"</td></tr>"
+ 	table=table+"<tr data-row="+i+"><td class='notStat'><img id='rep"+i+"' src='img/return.png' width='30'></td><td>"+moment(nArr[1]).format('DD.MM, HH:mm')+"</td><td>"+nArr[0]+"</td><td>"+(nArr.length-1)+"</td></tr>"
 }
 table=table+"<table>"
 var arrIndex=0;
 $('.tablePlace').html(table)
-$('td').on('click', function(){
+$('td:not(.notStat)').on('click', function(e){
 	arrIndex=$(this).parent().data('row')
-	console.log(arrIndex)
-	document.location.href='way.html'
+	modalWay(arrIndex)
 })
 
-$('.wayModal').css('left', document.documentElement.clientWidth/2-100)
+
+$('td').on('swipe', function(){
+	console.log($(this).parent().data('row'))
+	deleteRow($(this).parent())
+})
+function deleteRow(tr){
+	e=tr.data('row')
+	if(confirm('Удалить запись '+starArr[e].split(',')[0]+' от '+moment(starArr[e].split(',')[1]).format("DD.MM.YY HH:mm:ss")+'?')){
+		console.log('удаляем '+starArr[e][0]+'')
+		starArr.splice(e, 1)
+		tr.hide()
+		/*Удалем переменную счетчика из локального хранилища*/
+		var newStr='';
+		for(var i in starArr)
+			{
+				newStr=newStr+"/%/"+starArr[i]
+			}
+		localStorage.removeItem('justCounter')	
+		localStorage.setItem('justCounter', newStr)	
+		/*Удалем переменную счетчика из локального хранилища*/
+	}
+}
+
+$('img').click(function(){
+	var tmpId=this.id
+	var idToChange=tmpId.slice(3, tmpId.length)
+	console.log("Поправим массив "+idToChange)
+	localStorage.setItem('idToChange', idToChange)
+	document.location.href="screen1.html"
+})
+
+//модальное окно
+function modalWay(a){
+	$('.wayModalBody').html('')
+	$('.statHover, .wayModal').fadeIn()
+	$('.wayModal').css('left', document.documentElement.clientWidth/2-150)
+	var h=document.documentElement.clientHeight-80;
+	$('.way').css('height', h)
+
+
+console.log(a)
+	
+	//тестовый массив
+		var nn=starArr[a].split(',')
+		
+		var wayHead=nn[0]
+		$('.wayModalHeader').html(nn[0])
+		nn.shift()
+		nn.shift()
+		console.log(nn)
+
+		//конец тестового массива
+		var l=nn[nn.length-1]-nn[0]
+		$('.wayModalHeader').html(wayHead+' ('+getDuration(l)+')')
+		var pp=((h-30*nn.length)/l)
+		console.log(pp)
+		console.log(l)
+		var allMargin=0
+		for(i in nn){
+		  if (i>0){
+		    var d=(nn[i]-nn[parseInt(i)-1]);
+		    console.log("margin: "+d*pp)
+		    allMargin=allMargin+d*pp
+		    var durationStr=getDuration(parseInt(nn[i])-parseInt(nn[parseInt(i)-1]))+" ("+getDuration(parseInt(nn[i])-parseInt(nn[0]))+")" 
+		    $('.wayModalBody').append('<div class="dotHolder" style="margin-top:'+(d*pp)+'px"><div class="dot"><p>'+(parseInt(i)+1)+'</p></div><div class="dotLabel">+'+durationStr+'</div></div>')
+		  }else{
+		    $('.wayModalBody').append('<div class="dotHolder"><div class="dot"><p>'+(parseInt(i)+1)+'</p></div><div class="dotLabel">'+moment(parseInt(nn[i])).format('DD.MM - HH:mm:ss')+'</div></div>')
+		  } 
+		}
+}
+
+$('.statHover').click(function(){
+	$('.statHover, .wayModal').fadeOut()
+})
+function getDuration(l){
+	if(l<60000){
+		return(moment(l).format('ss.SSS сек.'))
+	}
+	if(60000<l<3600000){
+		return(moment(l).format('mm мин., ss сек.'))
+	}
+	if(3600000<l<3600000*24){
+		return(moment(l).format('HH ч., mm мин., ss сек.'))
+	}
+	if(3600000*24*365<l){
+		return(moment(l).format('DD д., HH ч., mm мин., ss сек.'))
+	}
+}
+$.mobile.loading().hide();
 
